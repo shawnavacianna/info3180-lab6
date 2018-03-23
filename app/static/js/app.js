000d4ae1b1ccaf0314a50ea2b6d94b2f988c91d2
ShawnaVacianna <shawnkay.vacianna@mymona.uwi.edu>
2018-03-23 21:27:29 +0000

    List of Articles, News Component

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -39,6 +39,36 @@ Vue.component('app-footer', {
     }
 })
 
+Vue.component('news-list',{
+    template: `
+    <div class="news">
+        <h2>News</h2>
+        <ul class="news__list">
+            <li v-for="article in articles"
+            class="news__item">{{ article.urlToImage }}</li>
+        </ul>
+    </div>`,
+    created: function() 
+    {
+        let self = this;
+        
+        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=')
+            .then(function(response){  
+                return response.json();           
+                })           
+            .then(function(data){ 
+                    console.log(data);
+                    self.articles = data.articles;        
+                });     
+    },
+    data: function()
+    {  
+        return {
+            articles: []
+        }     
+    } 
+    
+});
 
 let app = new Vue({
     el: '#app',
