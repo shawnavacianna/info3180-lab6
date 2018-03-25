e8e6fa9f1c4bb08551bd42e2b23d0214ed6b72bf
ShawnaVacianna <shawnkay.vacianna@mymona.uwi.edu>
2018-03-25 19:33:13 +0000

    Added Style, Finished

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -54,33 +54,30 @@ const Home = Vue.component('home', {
 });
 
 
-const NewsList =Vue.component('news-list',{
-    template: `
-    <div class="news">
-        <h2>News</h2>
-        <ul class="news__list">
-            <li v-for="article in articles"
-            class="news__item">
-                <img v-bind:src="article.urlToImage"/>
-                <label>{{article.title}}</label>
-                <p>{{article.description}}</p></li>
-        </ul>
-        <div class="form-inline d-flex justify-content-center">
-            <div class="form-group mx-sm-3 mb-2">
-                <label class="sr-only" for="search">Search</label>
-                <input type="search" name="search" v-model="searchTerm"
-                id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter
-                search term here" />
-                <button class="btn btn-primary mb-2"
-                @click="searchNews">Search</button>
-            </div>
-        </div>
-    </div>`,
+const NewsList = Vue.component('news-list', {
+    template: `<div class="news">
+                    <h2>News</h2>
+                    <div class="form-inline d-flex justify-content-center">
+                        <div class="form-group mx-sm-3 mb-2">
+                            <label class="sr-only" for="search">Search</label>
+                            <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+                            <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
+                        </div>
+                    </div>
+                    <div class="grid-form">
+                        <div v-for="article in articles" class="news_item grip-item">
+                            <h5>{{article.title}}</h5>
+                            <img class="news-img" v-bind:src="article.urlToImage"/>
+                            <p>{{article.description}}</p>
+                        </div>
+                    </div>
+                </div>
+                `,
     created: function() 
     {
         let self = this;
         
-        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=df39776fa3a84f73b858f491f87986da')
+        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=')
             .then(function(response){  
                 return response.json();           
                 })           
@@ -121,7 +118,7 @@ const router = new VueRouter({
     ]
 });
 
-let app = new Vue({
+const app = new Vue({
     el: '#app',
     router
 });
